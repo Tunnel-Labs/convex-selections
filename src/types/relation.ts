09c159aebddcc4a617578aba66b95aa5aec12e42
Leon Si <leon@leondreamed.com>
2023-10-27 19:59:45 -0400

    fixk

--- src/types/relation.ts
+++ src/types/relation.ts
@@ -1,6 +1,6 @@
 export type Relation<$TableName extends string> =
-	| `__RELATION__:${$TableName}`
+	| `__RELATION__:${$TableName}:${string}`
 	| undefined;
 export type RelationArray<$TableName extends string> =
-	| `__RELATION__:${$TableName}`[]
+	| `__RELATION__:${$TableName}:${string}`[]
 	| undefined;
--- src/utils/relation.ts
+++ src/utils/relation.ts
@@ -1,15 +1,38 @@
 import { v } from 'convex/values';
+import { GetIndexesFromTable } from '~/types/index.js';
 
-export function vRelation<$ForeignTableName extends string, $ForeignProperty>(
-	foreignTableName: $ForeignTableName,
-	_foreignProperty: $ForeignProperty
-) {
-	return v.optional(v.literal(`__RELATION__:${foreignTableName}`));
+export function vRelation<$ForeignTable>(foreignTableName: string) {
+	return {
+		withIndex<
+			$Index extends keyof GetIndexesFromTable<$ForeignTable> & string,
+			$IndexFields extends GetIndexesFromTable<$ForeignTable>[$Index]
+		>(index: $Index, indexFields: $IndexFields) {
+			return v.optional(
+				v.literal(
+					`__RELATION__:${foreignTableName}:${foreignTableName}:${index}:${indexFields.join(
+						','
+					)}`
+				)
+			);
+		}
+	};
 }
 
-export function vRelationArray<
-	$ForeignTableName extends string,
-	$ForeignProperty
->(foreignTableName: $ForeignTableName, _foreignProperty: $ForeignProperty) {
-	return v.optional(v.array(v.literal(`__RELATION__:${foreignTableName}`)));
+export function vRelationArray<$ForeignTable>(foreignTableName: $ForeignTable) {
+	return {
+		withIndex<
+			$Index extends keyof GetIndexesFromTable<$ForeignTable> & string,
+			$IndexFields extends GetIndexesFromTable<$ForeignTable>[$Index]
+		>(index: $Index, indexFields: $IndexFields) {
+			return v.optional(
+				v.array(
+					v.literal(
+						`__RELATION__:${foreignTableName}:${foreignTableName}:${index}:${indexFields.join(
+							','
+						)}`
+					)
+				)
+			);
+		}
+	};
 }
