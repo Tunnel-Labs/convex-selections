8467e505fee81588420881e6079f216898b8d5b9
Leon Si <leon@leondreamed.com>
2023-10-27 15:08:47 -0400

    fix: relation types

--- src/types/relation.ts
+++ src/types/relation.ts
@@ -1,1 +1,12 @@
-export type Relation<$TableName extends string> = `__RELATION__:${$TableName}`;
+import { Validator } from 'convex/values';
+
+export type Relation<$TableName extends string> = Validator<
+	`__RELATION__:${$TableName}` | undefined,
+	true,
+	never
+>;
+export type RelationArray<$TableName extends string> = Validator<
+	`__RELATION__:${$TableName}`[] | undefined,
+	true,
+	never
+>;
