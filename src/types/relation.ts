db0edaa0e9cab50004b997aac03fc23e82c10e07
Leon Si <leon@leondreamed.com>
2023-10-27 15:15:22 -0400

    fix

--- src/types/relation.ts
+++ src/types/relation.ts
@@ -1,12 +1,6 @@
-import { Validator } from 'convex/values';
-
-export type Relation<$TableName extends string> = Validator<
-	`__RELATION__:${$TableName}` | undefined,
-	true,
-	never
->;
-export type RelationArray<$TableName extends string> = Validator<
-	`__RELATION__:${$TableName}`[] | undefined,
-	true,
-	never
->;
+export type Relation<$TableName extends string> =
+	| `__RELATION__:${$TableName}`
+	| undefined;
+export type RelationArray<$TableName extends string> =
+	| `__RELATION__:${$TableName}`[]
+	| undefined;
--- src/utils/relation.ts
+++ src/utils/relation.ts
@@ -1,19 +1,15 @@
 import { v } from 'convex/values';
-import type { Relation, RelationArray } from '~/types/relation.js';
 
 export function vRelation<$ForeignTableName extends string, $ForeignProperty>(
 	foreignTableName: $ForeignTableName,
 	_foreignProperty: $ForeignProperty
-): Relation<$ForeignTableName> {
+) {
 	return v.optional(v.literal(`__RELATION__:${foreignTableName}`));
 }
 
 export function vRelationArray<
 	$ForeignTableName extends string,
 	$ForeignProperty
->(
-	foreignTableName: $ForeignTableName,
-	_foreignProperty: $ForeignProperty
-): RelationArray<$ForeignTableName> {
+>(foreignTableName: $ForeignTableName, _foreignProperty: $ForeignProperty) {
 	return v.optional(v.array(v.literal(`__RELATION__:${foreignTableName}`)));
 }
