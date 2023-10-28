fb4f4c4328e3ef6755a827beecf5be694e439ce6
Leon Si <leon@leondreamed.com>
2023-10-28 10:31:28 -0400

    fix

--- src/utils/relation.ts
+++ src/utils/relation.ts
@@ -1,24 +1,9 @@
 import { v } from 'convex/values';
-import { GetIndexesFromTable } from '~/types/index.js';
 
-export function vRelation<$ForeignTable>(foreignTableName: string) {
-	return {
-		withIndex<$Index extends keyof GetIndexesFromTable<$ForeignTable> & string>(
-			index: $Index
-		) {
-			return v.optional(v.literal(`__RELATION__:${foreignTableName}:${index}`));
-		}
-	};
+export function vRelation(foreignTableName: string) {
+	return v.optional(v.literal(`__RELATION__:${foreignTableName}`));
 }
 
-export function vRelationArray<$ForeignTable>(foreignTableName: string) {
-	return {
-		withIndex<$Index extends keyof GetIndexesFromTable<$ForeignTable> & string>(
-			index: $Index
-		) {
-			return v.optional(
-				v.array(v.literal(`__RELATION__:${foreignTableName}:${index}`))
-			);
-		}
-	};
+export function vRelationArray(foreignTableName: string) {
+	return v.optional(v.array(v.literal(`__RELATION__:${foreignTableName}`)));
 }
