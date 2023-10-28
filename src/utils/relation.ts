607f3fe5a352cfeea4cb1da011119d7e0941fd41
Leon Si <leon@leondreamed.com>
2023-10-28 09:02:13 -0400

    fix

--- src/utils/relation.ts
+++ src/utils/relation.ts
@@ -3,41 +3,21 @@ import { GetIndexesFromTable } from '~/types/index.js';
 
 export function vRelation<$ForeignTable>(foreignTableName: string) {
 	return {
-		withIndex<
-			$Index extends keyof GetIndexesFromTable<$ForeignTable> & string,
-			$IndexFields extends GetIndexesFromTable<$ForeignTable>[$Index]
-		>(index: $Index, indexFields: $IndexFields) {
-			return v.optional(
-				v.literal(
-					`__RELATION__:${foreignTableName}:${foreignTableName}:${index}:${indexFields.join(
-						','
-					)}`
-				)
-			);
+		withIndex<$Index extends keyof GetIndexesFromTable<$ForeignTable> & string>(
+			index: $Index
+		) {
+			return v.optional(v.literal(`__RELATION__:${foreignTableName}:${index}`));
 		}
 	};
 }
 
 export function vRelationArray<$ForeignTable>(foreignTableName: string) {
 	return {
-		withIndex<
-			$Index extends keyof GetIndexesFromTable<$ForeignTable> & string,
-			$IndexFields extends GetIndexesFromTable<$ForeignTable>[$Index],
-			$IndexFieldsWithoutLast extends $IndexFields extends [
-				...infer $IndexFieldsWithoutLast,
-				infer _$LastIndexField
-			]
-				? $IndexFieldsWithoutLast
-				: never
-		>(index: $Index, indexFields: $IndexFieldsWithoutLast) {
+		withIndex<$Index extends keyof GetIndexesFromTable<$ForeignTable> & string>(
+			index: $Index
+		) {
 			return v.optional(
-				v.array(
-					v.literal(
-						`__RELATION__:${foreignTableName}:${foreignTableName}:${index}:${indexFields.join(
-							','
-						)}`
-					)
-				)
+				v.array(v.literal(`__RELATION__:${foreignTableName}:${index}`))
 			);
 		}
 	};
