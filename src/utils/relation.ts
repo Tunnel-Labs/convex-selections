3f864f55deaadcd322b00665ee3f00a3cbb443d6
Leon Si <leon@leondreamed.com>
2023-10-30 15:42:48 -0400

    fix: make relations v.any()

--- src/utils/relation.ts
+++ src/utils/relation.ts
@@ -1,9 +1,13 @@
-import { v } from 'convex/values';
+import { Validator, v } from 'convex/values';
 
-export function vRelation<$TableName extends string>(foreignTableName: $TableName) {
-	return v.optional(v.literal(`__RELATION__:${foreignTableName}`));
+export function vRelation<$TableName extends string>(
+	foreignTableName: $TableName
+): Validator<`__RELATION__:${$TableName}`, false, never> {
+	return Object.assign(v.optional(v.any()), { __relation__: true }) as any;
 }
 
-export function vRelationArray<$TableName extends string>(foreignTableName: $TableName) {
-	return v.optional(v.array(v.literal(`__RELATION__:${foreignTableName}`)));
+export function vRelationArray<$TableName extends string>(
+	foreignTableName: $TableName
+): Validator<`__RELATION__:${$TableName}`[] | undefined, true, never> {
+	return Object.assign(v.optional(v.any()), { __relationArray__: true }) as any;
 }
