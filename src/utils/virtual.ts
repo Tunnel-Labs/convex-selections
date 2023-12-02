760a89be76c7517b90e0e456bd38bad7275bb9a3
Leon Si <leon@leondreamed.com>
2023-12-02 07:49:29 -0500

    fix

--- src/utils/virtual.ts
+++ src/utils/virtual.ts
@@ -4,16 +4,16 @@ import { Validator, v } from 'convex/values';
 export function vVirtual<$TableName extends string>(
 	foreignTableName: $TableName,
 	options: { nullable: true }
-): Validator<`__RELATION__:${$TableName}` | null | undefined, true, never>;
+): Validator<`__VIRTUAL__:${$TableName}` | null | undefined, true, never>;
 export function vVirtual<$TableName extends string>(
 	foreignTableName: $TableName
-): Validator<`__RELATION__:${$TableName}` | undefined, true, never>;
+): Validator<`__VIRTUAL__:${$TableName}` | undefined, true, never>;
 export function vVirtual(foreignTableName: string): any {
-	return Object.assign(v.optional(v.any()), { __relation__: true }) as any;
+	return Object.assign(v.optional(v.any()), { __virtual__: true }) as any;
 }
 
 export function vVirtualArray<$TableName extends string>(
 	foreignTableName: $TableName
-): Validator<`__RELATION__:${$TableName}`[] | undefined, true, never> {
-	return Object.assign(v.optional(v.any()), { __relationArray__: true }) as any;
+): Validator<`__VIRTUAL__:${$TableName}`[] | undefined, true, never> {
+	return Object.assign(v.optional(v.any()), { __virtualArray__: true }) as any;
 }
