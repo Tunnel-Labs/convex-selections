a7dfcc2a95ec99c7bdeb33167c06d47b20516df3
Leon Si <leon@leondreamed.com>
2023-12-20 11:11:35 -0500

    refactor: corvex

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,18 @@ export {
 	combineSelections
 } from './utils/selection.js';
 export { createWithSelection } from './utils/with-selection.js';
+export { virtual, virtualArray } from './utils/relation.js';
+export { vNullable } from './validators/nullable.js';
+export { table } from './utils/table.js';
+export { vVirtual, vVirtualArray } from './validators/virtual.js';
+export { vDeprecated, vDeprecatedTable, vNew } from './validators/variant.js';
+
+export type * from './types/convex.js';
+export type * from './types/index.js';
 export type * from './types/procedure.js';
 export type * from './types/schema.js';
-export type * from './types/selections.js';
 export type * from './types/select.js';
+export type * from './types/selections.js';
+export type * from './types/table.js';
+export type * from './types/variant.js';
 export type * from './types/virtual.js';
--- src/validators.ts
+++
@@ -1,3 +0,0 @@
-export { vNullable } from './validators/nullable.js';
-export { vVirtual, vVirtualArray } from './validators/virtual.js';
-export { vDeprecated, vDeprecatedTable } from './validators/variant.js';
